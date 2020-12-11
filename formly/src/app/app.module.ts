c3ce1baa52e6a778ecd82724be7458a2262e6b42
Borges <fabio.borges@nfon.com>
2020-12-11 14:14:44 +0000

    material update, mock form

--- formly/src/app/app.module.ts
+++ formly/src/app/app.module.ts
@@ -3,24 +3,48 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+} from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormlyModule } from '@ngx-formly/core';
 import { FormlyMaterialModule } from '@ngx-formly/material';
+import { FormComponent } from './form/form.component';
+import { IpValidator, IpValidatorMessage } from './utils/validators';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MAT_FORM_FIELD_DEFAULT_OPTIONS,
+  MatFormFieldDefaultOptions,
+} from '@angular/material/form-field';
+
+const appearance: MatFormFieldDefaultOptions = {
+  appearance: 'outline',
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, FormComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatButtonModule,
     ReactiveFormsModule,
-    FormlyModule.forRoot({ extras: { lazyRender: true } }),
-    FormlyMaterialModule
+    FormlyModule.forRoot({
+      validators: [{ name: 'ip', validation: IpValidator }],
+      validationMessages: [
+        { name: 'required', message: 'This field is required' },
+        { name: 'ip', message: IpValidatorMessage },
+      ],
+    }),
+    FormlyMaterialModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: appearance,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
